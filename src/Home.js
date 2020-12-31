f265737ab86b61cf51f1aac54f03b949cdb027a4
Pijano97 <gadzic.dimitrije97@gmail.com>
2020-12-31 11:03:17 +0100

    header done

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+	return (
+		<div className="home">
+			<div className="home__container">
+				<img
+					className="home__image"
+					src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBh-3QUQk5D8_aWh4t_RAvu1a-X8Nl0I0Q",
+	authDomain: "fullclone-74a4e.firebaseapp.com",
+	projectId: "fullclone-74a4e",
+	storageBucket: "fullclone-74a4e.appspot.com",
+	messagingSenderId: "506589059611",
+	appId: "1:506589059611:web:d705fc90068cae358436be",
+	measurementId: "G-ENV5F11MQX",
+};
