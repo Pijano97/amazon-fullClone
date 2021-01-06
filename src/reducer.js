8f2cbb2279f5647b4d7010bbe0c844d602f27870
Pijano97 <gadzic.dimitrije97@gmail.com>
2021-01-06 20:35:33 +0100

    checkout page completed

--- src/reducer.js
+++ src/reducer.js
@@ -2,12 +2,33 @@ export const initialState = {
 	basket: [],
 };
 
+// Selector - go trough whole basket, for each item add price to amount( amount is set to 0)
+export const getBasketTotal = (basket) =>
+	basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
 	switch (action.type) {
 		case "ADD_TO_BASKET":
 			return {
 				...state,
 				basket: [...state.basket, action.item],
+			};
+		case "REMOVE_FROM_BASKET":
+			const index = state.basket.findIndex(
+				(basketItem) => basketItem.id === action.id
+			);
+			let newBasket = [...state.basket];
+			// cut one from index
+			if (index >= 0) {
+				newBasket.splice(index, 1);
+			} else {
+				console.warn(
+					"Cant remove product (id: ${action.id}) as its not in basket!"
+				);
+			}
+			return {
+				...state,
+				basket: newBasket,
 			};
 		default:
 			return state;
